f1a0a1230f306811054a5ab028eb3c7d35c43d71
agentmess <peder.larson@ucsf.edu>
2018-03-14 22:09:20 -0700

    Updated for TRAMP sample data fits

--- sample_data/demo_sample_data_fit.m
+++ sample_data/demo_sample_data_fit.m
@@ -3,23 +3,52 @@ clear all
 
 %% Load data
 
-% uncomment data to test
-%datafile = 'TRAMP Mouse multislice EPI/exp_tramp_epi.mat'; zplot = 10; SNR_thresh = 1e5;
-datafile = 'Rat Kidneys EPI/exp1_vfa/exp1_vfa.mat'; zplot = 1; SNR_thresh = 1e5;
-%datafile = 'Rat Kidneys EPI/exp2_constant/exp2_constant.mat'; zplot = 1; SNR_thresh = 1e5;
+
+I_sample_data = 2;
+
+switch I_sample_data
+    case 1
+        datafile = 'Rat Kidneys EPI/exp1_vfa/exp1_vfa.mat'; zplot = 1; SNR_thresh = 1e5;
+        phase_flag = 0;
+    case 2
+        datafile = 'Rat Kidneys EPI/exp2_constant/exp2_constant.mat'; zplot = 1; SNR_thresh = 1e5;
+        phase_flag = 0;
+    case 3
+        datafile = 'TRAMP Mouse multislice EPI/exp_tramp_epi.mat'; zplot = 8; SNR_thresh = 1e5;
+        phase_flag = 1;
+end
 
 load(datafile)
 flips_all = [flips_pyr(:).';flips_lac(:).'] *pi/180;
 size_pyr= size(pyr);
 Nx_dims = length(size_pyr)-1;
+% convert to [x,y,z,t]
+if Nx_dims ==2
+    pyr = reshape(pyr, [size_pyr(1:Nx_dims), 1, size_pyr(end)]);
+    lac = reshape(lac, [size_pyr(1:Nx_dims), 1, size_pyr(end)]);
+end
 
-AUC_pyr = sum(pyr,Nx_dims+1);
-AUC_lac = sum(lac,Nx_dims+1);
+size_pyr= size(pyr);
+
+% phase data based on dynamics
+if phase_flag
+    for x = 1:size_pyr(1)
+        for y = 1:size_pyr(2)
+            for z= 1:size_pyr(3)
+                pyr(x,y,z,:) =  pyr(x,y,z,:) * exp(i*find_phase_corr(squeeze(pyr(x,y,z,:))));
+                lac(x,y,z,:) =  lac(x,y,z,:) * exp(i*find_phase_corr(squeeze(lac(x,y,z,:))));
+            end
+        end
+    end
+    
+end
+
+AUC_pyr = real(sum(pyr,4));
+AUC_lac = real(sum(lac,4));
 AUC_ratio = AUC_lac./AUC_pyr;
 
-pyr = reshape(pyr, [size_pyr(1:Nx_dims), 1, size_pyr(end)]);
-lac = reshape(lac, [size_pyr(1:Nx_dims), 1, size_pyr(end)]);
-data = cat(Nx_dims+1, pyr, lac);
+data = cat(4, reshape(real(pyr), [size_pyr(1:3), 1, size_pyr(4)]), ...
+    reshape(real(lac), [size_pyr(1:3), 1, size_pyr(4)]) );
 
 
 
@@ -34,7 +63,7 @@ params_est.kPL = kPL_est;
 SNRmask = AUC_pyr > SNR_thresh;
 It_fit = find(flips_lac > 1);
 
-[params_fit Sfit] = fit_kPL(double(data(:,:,:,It_fit)), TR, flips_all(:, It_fit), params_fixed, params_est);
+[params_fit Sfit] = fit_kPL(double(data(:,:,:,:,It_fit)), TR, flips_all(:, It_fit), params_fixed, params_est);
 
 figure
 subplot(221)
