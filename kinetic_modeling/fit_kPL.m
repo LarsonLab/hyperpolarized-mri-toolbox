b6dcae883703eacefd4d37a82bcd07f19c18cf88
agentmess <peder.larson@gmail.com>
2018-10-18 16:38:07 -0700

    minor fit_kPL fix

--- kinetic_modeling/fit_kPL.m
+++ kinetic_modeling/fit_kPL.m
@@ -158,9 +158,11 @@ for i=1:size(S, 1)
         ufit(i,:) = ufit(i,:)  .* Sscale(1, :);
         
         % export goodness of fit parameters (ub, lb, total error, R^2, chi^2)
+        if exist('sigma')
         err_vec(i,1)=sigma(1,2);
         err_vec(i,2)=sigma(1,1);
         err_vec(i,3)=sigma(1,2)-sigma(1,1);
+        end
         err_vec(i,4)=1-mean((S(i,2,:)-Sfit(i,2,:)).^2./S(i,2,:).^2);
         err_vec(i,5)=sum((S(i,2,:)-Sfit(i,1,:)).^2);
         
@@ -215,7 +217,7 @@ end
 if length(Nx) > 1
     for n = 1:length(err_all)
         param_name = err_all{(n)};
-        err_metrics.(param_name) = reshape(err_metrics.(param_name), [12,12,16]);
+        err_metrics.(param_name) = reshape(err_metrics.(param_name), Nx);
     end
 end
 
