84521d83bef81634f79a75ac1d9e55d93e49971f
Peder Larson <peder.larson@ucsf.edu>
2019-08-26 22:52:25 -0700

    converted with input fitting to new function
    Removed all old fit_kPL*input and associated trajectories_* and test_fit*
    new test script as well, confirmed it is working reasonably well for this

--- kinetic_modeling/fit_pyr_kinetics_and_input.m
+++ kinetic_modeling/fit_pyr_kinetics_and_input.m
@@ -1,17 +1,14 @@
-function [params_fit, Sfit, ufit, error_metrics] = fit_pyr_kinetics(S, TR, flips, params_fixed, params_est, noise_level, plot_flag)
+function [params_fit, Sfit, ufit, error_metrics] = fit_pyr_kinetics_and_input(S, TR, flips, params_fixed, params_est, noise_level, plot_flag)
 % fit_pyr_kinetics - Kinetic model fitting function for HP 13C MRI.
 %
-% Fits product signals, assuming origination from a single substrate
-% In other words, pyruvate to lactate, bicarbonate and alanine.
-% An "input-less" method is used, eliminating
-% need to make any assumptions about the input function.
+% Fits substrate (pyruvate) and product signals, assuming a given input function shape.
 % This uses the following assumptions:
 %   - uni-directional conversion from substrate to metabolic products (i.e.
 %   pyruvate to lactate)
 % It also allows for fixing of parameters. Based on simulations, our
 % current recommendation is to fix pyruvate T1, as it doesn't impact kPX substantially.
 %
-% [params_fit, Sfit, ufit, objective_val] = fit_pyr_kinetics(S, TR, flips, params_fixed, params_est, noise_level, plot_flag)
+% [params_fit, Sfit, ufit, objective_val] = fit_pyr_kinetics_and_input(S, TR, flips, params_fixed, params_est, noise_level, plot_flag)
 %
 % All params_* values are structures, including possible fields of 'kPL', 'kPB', 'kPA', (1/s),
 % 'R1P', 'R1L', 'R1B', 'R1A' (1/s).
@@ -45,6 +42,8 @@ function [params_fit, Sfit, ufit, error_metrics] = fit_pyr_kinetics(S, TR, flips
 
 isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 
+global input_shape;
+input_shape = 'gamma'; %input_shape = 'boxcar';
 
 size_S = size(S);  ndimsx = length(size_S)-2;
 Nt = size_S(end); t = [0:Nt-1]*TR;
@@ -56,20 +55,16 @@ end
 
 params_all = {'kPL', 'kPB', 'kPA', ...
     'R1P', 'R1L', 'R1A', 'R1B', ...
-    'S0_L', 'S0_B', 'S0_A'};%, ...
-%    'Rinj', 'Tarrival', 'Tbolus'};
+    'Rinj', 'Tarrival', 'Tbolus'};
 params_default_est = [0.01, 0.01, 0.01, ...
     1/30, 1/25, 1/25, 1/15, ...
-    0, 0, 0] ;%, ...
-%    0.1, 0, 8];
+    0.1, 0, 8];
 params_default_lb = [-Inf, -Inf, -Inf, ...
     1/50, 1/50, 1/50, 1/50, ...
-    -Inf, -Inf, -Inf]; %, ...
-%    0, -30, 0];
+    0, -30, 0];
 params_default_ub = [Inf, Inf, Inf, ...
     1/10, 1/10, 1/10, 1/5 , ...
-    Inf, Inf, Inf]; %, ...
-%    Inf 30 Inf];
+    Inf 30 Inf];
 
 
 if nargin < 5 || isempty(params_fixed)
@@ -81,15 +76,15 @@ if nargin < 6 || isempty(params_est)
 end
 
 % Supports up to 3 metabolic products (e.g. alanine, lactate, bicarb)
-products_string = {'lactate', 'bicarb', 'alanine'};
+products_string = {'pyruvate', 'lactate', 'bicarb', 'alanine'};
 switch Nmets
     case 2 % assume pyruvate & lactate
         params_fixed.kPA = 0;  params_fixed.S0_A = 0;  params_fixed.R1A = 1;
         params_fixed.kPB = 0;  params_fixed.S0_B = 0;  params_fixed.R1B = 1;
-        products_string = {'lactate'};
+        products_string = {'pyruvate', 'lactate'};
     case 3 % assume pyruvate & lactate & bicarbonate
         params_fixed.kPA = 0;   params_fixed.S0_A = 0;  params_fixed.R1A = 1;
-        products_string = {'lactate', 'bicarb'};
+        products_string = {'pyruvate', 'lactate', 'bicarb'};
 end
 
 
@@ -149,7 +144,7 @@ end
 [Sscale, Mzscale] = flips_scaling_factors(flips, Nt);
 
 params_fit_vec = zeros([prod(Nx),Nparams_to_fit]);  objective_val = zeros([1,prod(Nx)]);
-Sfit = zeros([prod(Nx),Nmets-1,Nt]); ufit = zeros([prod(Nx),Nt]);
+Sfit = zeros([prod(Nx),Nmets,Nt]); ufit = zeros([prod(Nx),Nt]);
 
 
 for i=1:size(Sreshape, 1)
@@ -168,13 +163,12 @@ for i=1:size(Sreshape, 1)
         Istart = 1;
         
         % fit to data
-        % options = optimoptions(@fminunc,'Display','none','Algorithm','quasi-newton'); % not supported in Octave
-        options = optimset('Display','none','Algorithm','quasi-newton');
+        options = optimset('Display','none','Algorithm','quasi-newton'); % not supported in Octave?
         lsq_opts = optimset('Display','none','MaxIter', 500, 'MaxFunEvals', 500);
         
         switch(fit_method)
             case 'ls'
-                obj = @(var) difference_inputless(var, params_fixed, TR, Mzscale, Sscale, Mz, Istart, Nmets) ;  % perform least-squares in signal domain
+                obj = @(var) difference_all(var, params_fixed, TR, Mzscale, Sscale, Mz, Nmets) ;  % perform least-squares in signal domain
                 [params_fit_vec(i,:),objective_val(i),resid,~,~,~,J] = lsqnonlin(obj, params_est_vec, params_lb, params_ub, lsq_opts);
     
                 if ~isOctave
@@ -185,32 +179,35 @@ for i=1:size(Sreshape, 1)
                 err(i,:) = CI(:,2)-CI(:,1);
                 end                
             case 'ml'
-                obj = @(var) negative_log_likelihood_rician_inputless(var, params_fixed, TR, Mzscale, Mz, noise_level.*(Sscale).^2, Istart, Nmets);
-                [params_fit_vec(i,:), objective_val(i)] = fminunc(obj, params_est_vec, options);
+                obj = @(var) negative_log_likelihood_rician(var, params_fixed, TR, Mzscale, Mz, noise_level.*(Sscale).^2, Nmets);
+                [params_fit_vec(i,:), objective_val(i)] = fminunc(obj, params_est_vec,options); %, 'Display','none','Algorithm','quasi-newton'); % octave commented out
         end
         
-        [Mzfit, ufit(i,:)] = trajectories_inputless(params_fit_vec(i,:), params_fixed, TR,  Mzscale, Mz(1,:), Istart);
-        
-        Sfit(i,:,:) = Mzfit(2:Nmets,:)  .* Sscale(2:Nmets, :);
+        switch input_shape
+    case 'gamma'
+        [Mzfit, ufit(i,:)] = trajectories_gamma_input(params_fit_vec(i,:), params_fixed, TR,  Mzscale) ;        
+    case 'boxcar'
+        [Mzfit, ufit(i,:)] = trajectories_boxcar_input(params_fit_vec(i,:), params_fixed, TR,  Mzscale) ;
+end
+
+        Sfit(i,:,:) = Mzfit(1:Nmets,:)  .* Sscale(1:Nmets, :);
         ufit(i,:) = ufit(i,:)  .* Sscale(1, :);
         
-        % Note that Sfit only inlcudes products signals, not pyruvate,
-        % hence the difference in indexing here and in plots below
-        Rsq(i,1:Nmets-1) = 1 - sum( (Sreshape(i,2:Nmets,:)-Sfit(i,:,:)).^2 ,3 ) ...
-            ./ sum( (Sreshape(i,2:Nmets,:) - repmat(mean(Sreshape(i,2:Nmets,:),3),[1 1 Nt])).^2, 3);
+        Rsq(i,1:Nmets) = 1 - sum( (Sreshape(i,1:Nmets,:)-Sfit(i,:,:)).^2 ,3 ) ...
+            ./ sum( (Sreshape(i,1:Nmets,:) - repmat(mean(Sreshape(i,1:Nmets,:),3),[1 1 Nt])).^2, 3);
         % chi squared distance - also pdist2 in Octave
-        CHIsq(i,1:Nmets-1) = sum( (Sreshape(i,2:Nmets,:)-Sfit(i,:,:)).^2 ./ ...
-            (Sreshape(i,2:Nmets,:)+Sfit(i,:,:)) ,3) / 2;
+        CHIsq(i,1:Nmets) = sum( (Sreshape(i,1:Nmets,:)-Sfit(i,:,:)).^2 ./ ...
+            (Sreshape(i,1:Nmets,:)+Sfit(i,:,:)) ,3) / 2;
         
         if plot_flag
             % plot of fit for debugging
             figure(99)
             subplot(2,1,1)
-            plot(t, Mz(1:Nmets,:), t, Mzfit(2:Nmets,:),'--', t, ufit(i,:)./ Sscale(1, :), 'k:')
+            plot(t, Mz(1:Nmets,:), t, Mzfit(1:Nmets,:),'--', t, ufit(i,:)./ Sscale(1, :), 'k:')
             xlabel('time (s)')
             ylabel('state magnetization (au)')
             subplot(2,1,2)
-            plot(t, Mxy(1:Nmets,:), t, squeeze(Sfit(i,1:Nmets-1,:)),'--', t, ufit(i,:), 'k:')
+            plot(t, Mxy(1:Nmets,:), t, squeeze(Sfit(i,1:Nmets,:)),'--', t, ufit(i,:), 'k:')
             xlabel('time (s)')
             ylabel('signal (au)')
             
@@ -221,12 +218,11 @@ for i=1:size(Sreshape, 1)
             title(fit_results_string)
             disp(fit_results_string)
             
-            products_legend{1} = 'pyruvate';
-            for n = 1:Nmets-1
-                products_legend{n+1} = products_string{n};
+            for n = 1:Nmets
+                products_legend{n} = products_string{n};
                 products_legend{n+Nmets} = [products_string{n} ' fit'];
             end    
-            products_legend{Nmets*2} = 'input estimate';
+            products_legend{Nmets*2+1} = 'input estimate';
             legend( products_legend)
             drawnow, pause(0.5)
         end
@@ -274,29 +270,43 @@ end
 
 end
 
-function diff_products = difference_inputless(params_fit, params_fixed, TR, Mzscale, Sscale, Mz, Istart, Nmets)
-Mzfit = trajectories_inputless(params_fit, params_fixed, TR,  Mzscale, Mz(1,:), Istart) ;
+function diff_products = difference_all(params_fit, params_fixed, TR, Mzscale, Sscale, Mz, Nmets)
+global input_shape
+
+switch input_shape
+    case 'gamma'
+        Mzfit = trajectories_gamma_input(params_fit, params_fixed, TR,  Mzscale) ;        
+    case 'boxcar'
+        Mzfit = trajectories_boxcar_input(params_fit, params_fixed, TR,  Mzscale) ;
+end
+
 temp_diff = (Mz - Mzfit) .* Sscale; % compute difference in the signal (Mxy) domain
-diff_products = temp_diff(2:Nmets,:); % only differences are in the metabolic products
+diff_products = temp_diff(1:Nmets,:); % compute difference only for metabolites being fit
 diff_products = diff_products(:);
 end
 
-function [ l1 ] = negative_log_likelihood_rician_inputless(params_fit, params_fixed, TR, Mzscale, Mz, noise_level, Istart, Nmets)
+function [ l1 ] = negative_log_likelihood_rician(params_fit, params_fixed, TR, Mzscale, Mz, noise_level, Nmets)
 %FUNCTION NEGATIVE_LOG_LIKELIHOOD_RICIAN Computes log likelihood for
 %    compartmental model with Rician noise
 % noise level is scaled for state magnetization (Mz) domain
 
 %https://doi.org/10.1109/42.712125
 
+global input_shape
 N = size(Mzscale,2);
 
 % compute trajectory of the model with parameter values
-Mzfit = trajectories_inputless(params_fit, params_fixed, TR,  Mzscale, Mz(1,:), Istart) ;
+switch input_shape
+    case 'gamma'
+        Mzfit = trajectories_gamma_input(params_fit, params_fixed, TR,  Mzscale) ;        
+    case 'boxcar'
+        Mzfit = trajectories_boxcar_input(params_fit, params_fixed, TR,  Mzscale) ;
+end
 
 % compute negative log likelihood
 l1 = 0;
 for t = 1:N
-    for k = 2:Nmets
+    for k = 1:Nmets
         l1 = l1 - (...
             log(Mz(k, t)) - log(noise_level(k,t)) ...
             - (Mz(k, t)^2 + Mzfit(k, t)^2)/(2*noise_level(k,t)) ...
@@ -307,15 +317,10 @@ for t = 1:N
 end
 end
 
-function [Mz_all, u] = trajectories_inputless( params_fit, params_fixed, TR, Mzscale , Mz_pyr, Istart )
+function [Mz_all, u] = trajectories_boxcar_input( params_fit, params_fixed, TR, Mzscale )
 % Compute product magnetizations using a uni-directional two-site model
 % Uses substrate magnetization measurements, estimated relaxation and
 % conversion rates
-
-if nargin < 6
-    Istart = 1;
-end
-
 
 Nmets = size(Mzscale,1); N = size(Mzscale,2);
 Mz_all = zeros(Nmets, N);
@@ -323,7 +328,7 @@ u = zeros(1,N);
 
 params_all = {'kPL', 'kPB', 'kPA', ...
     'R1P', 'R1L', 'R1A', 'R1B', ...
-    'S0_L', 'S0_B', 'S0_A'};
+    'Rinj', 'Tarrival', 'Tbolus'};
 
 nfit = 0;
 for n = 1:length(params_all)
@@ -335,51 +340,120 @@ for n = 1:length(params_all)
     end
 end
 
-Mz_all(1,:) = Mz_pyr;
-Mz_all(2,Istart) = S0_L;
-Mz_all(3,Istart) = S0_B;
-Mz_all(4,Istart) = S0_A;
+A = [-R1P-kPL-kPB-kPA, 0, 0, 0
+    +kPL, -R1L, 0, 0
+    +kPB, 0, -R1B, 0
+    +kPA, 0, 0, -R1A];
+
+for It=0:N-1
+    
+    t = It*TR;  % solving for Mz at time t (accounting for previous TR interval)
+    
+    if It == 0
+        Mz_init = zeros([4,1]);  % always start at no magnetization
+        if Tarrival < 0
+            % account for longer period of bolus prior to imaging
+            TRactual = -Tarrival;
+        else
+            TRactual = TR;
+        end
+        
+    else
+        Mz_init = Mz_all(:,It) .* Mzscale(:, It);
+        TRactual = TR;
+    end
+    
+    % determine if within boxcar input or not
+    if t <= Tarrival
+        % no arrival during TR
+        u(It+1) = 0;
+        Mz_all(:,It+1) = 0;
+        continue
+    elseif t-TRactual >= (Tarrival+Tbolus)
+        % after bolus arrival completed
+        u(It+1) = 0;
+    elseif t-TRactual < Tarrival && t > Tarrival
+        % TR interval contains arrival time
+        u(It+1) = Rinj * (t - Tarrival)/TRactual;
+    elseif t-TR < (Tarrival+Tbolus) && t > (Tarrival+Tbolus)
+        % TR interval contains end of bolus
+        u(It+1) =Rinj * (Tarrival+Tbolus - (t-TRactual))/TRactual;
+    else
+        % TR interval should be completely in bolus
+        u(It+1) = Rinj;
+    end
+    
+        
+    xstar = - inv(A)*[u(It+1),0,0,0].';
+    
+    % solve next time point under assumption of constant input during TR
+    Mz_all(:,It+1) = xstar + expm(A*TRactual) * (Mz_init - xstar);
+    
+    
+end
+
+end
+
+function [Mz_all, u] = trajectories_gamma_input( params_fit, params_fixed, TR, Mzscale )
+% Compute product magnetizations using a uni-directional two-site model
+% Uses substrate magnetization measurements, estimated relaxation and
+% conversion rates
+
+
+Nmets = size(Mzscale,1); N = size(Mzscale,2);
+Mz_all = zeros(Nmets, N);
+u = zeros(1,N);
+
+params_all = {'kPL', 'kPB', 'kPA', ...
+    'R1P', 'R1L', 'R1A', 'R1B', ...
+    'Rinj', 'Tarrival', 'Tbolus'};
+
+nfit = 0;
+for n = 1:length(params_all)
+    if isfield(params_fixed, params_all(n))
+        eval([params_all{n} '= params_fixed.(params_all{n});']);
+    else
+        nfit = nfit+1;
+        eval([params_all{n} '= params_fit(nfit);']);
+    end
+end
 
 A = [-R1P-kPL-kPB-kPA, 0, 0, 0
     +kPL, -R1L, 0, 0
     +kPB, 0, -R1B, 0
     +kPA, 0, 0, -R1A];
 
-for It=Istart:N-1
+% these parameters give a full-width half-max of the bolus of ~ Tbolus sec
+Agam = 4;
+Bgam = Tbolus/4;
+
+
+for It=0:N-1
     
-    Mz_init = Mz_all(:,It) .* Mzscale(:, It);
+    t = It*TR;  % solving for Mz at time t (accounting for previous TR interval)
     
-    % estimate input, assuming this is constant during TR interval
-    % This calculation could be improved for noise stability?
-    u(It) = ( Mz_pyr(It+1) - Mz_init(1)*exp((- R1P - kPL - kPB - kPA)*TR) ) * (R1P + kPL + kPB + kPA) / (1 - exp((- R1P - kPL - kPB - kPA)*TR));
+    if It == 0
+        Mz_init = zeros([4,1]);  % always start at no magnetization
+        if Tarrival < 0
+            % account for longer period of bolus prior to imaging
+            t_preimaging = [floor(Tarrival/TR):0]*TR; % t = 0
+            u(It+1) = sum( gampdf(t_preimaging-Tarrival,Agam,Bgam)*Rinj );
+        else
+            u(It+1) = gampdf(t-Tarrival,Agam,Bgam)*Rinj;
+        end
+        
+    else
+        Mz_init = Mz_all(:,It) .* Mzscale(:, It);
+        u(It+1) = gampdf(t-Tarrival,Agam,Bgam)*Rinj;
+    end
     
-    xstar = - inv(A)*[u(It),0,0,0].';
+        
+    xstar = - inv(A)*[u(It+1),0,0,0].';
     
     % solve next time point under assumption of constant input during TR
     Mz_all(:,It+1) = xstar + expm(A*TR) * (Mz_init - xstar);
     
     
 end
-
-% reverse in time
-for It=Istart:-1:2
-    
-    Mz_init = Mz_all(:,It);
-    
-    % estimate input, assuming this is constant during TR interval
-    % This calculation could be improved for noise stability?
-    u(It-1) = ( Mz_pyr(It-1)*Mzscale(1,It-1) - Mz_init(1)*exp((- R1P - kPL - kPB - kPA)*-TR) ) * (R1P + kPL + kPB + kPA) / (1 - exp((- R1P - kPL - kPB - kPA)*-TR));
-    
-    xstar = - inv(A)*[u(It-1),0,0,0].';
-    
-    % solve previous time point under assumption of constant input during TR
-    Mz_plus = xstar + expm(A*-TR) * (Mz_init - xstar);
-    
-    
-    Mz_all(:,It-1) = Mz_plus ./ Mzscale(:, It-1);
-    
-    
-end
-
 
 end
