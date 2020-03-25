a717179f74480996924bf5298c27e4f554d6d498
Peder Larson <peder.larson@ucsf.edu>
2020-03-25 11:43:16 -0700

    Squashed 'RF_pulses/spectral_spatial/' changes from 8478487..58b6d55
    58b6d55 Added DAD XML outputs to ss_save functions
    ebc3931 checkpoint commit on adding XML RF pulse output
    997d934 Updates for Bruker pulse file writing
    aa75494 Merge commit '4e8d78f4928b781a7c117b8093f7639c2cb28c7f'
    69c83db Merge branch 'master' of https://github.com/LarsonLab/hyperpolarized-mri-toolbox
    3dde55a Minor fix for dz2d()
    697652d merging changes for fixing GE pulse duration bug
    d9e9add fixed kPL error plots, and potential bug with spectral-spatial waveforms for odd number of points on GE systems
    
    git-subtree-dir: RF_pulses/spectral_spatial
    git-subtree-split: 58b6d556574edb4e1e21f455a9b462f96746339d

--- ss_save_dyn.m
+++ ss_save_dyn.m
@@ -2,8 +2,8 @@ function ss_save_dyn(g,rf,ang,thk, isodelay, format, fspec, a_angs, root_fname, 
 % SS_SAVE - Save spectral-spatial pulse 
 % Uses Chuck Cunningham's format for GE systems, and creates associated
 % .dat-file
-% Pulse parameters saved in header for Varian fules
-%   
+% Pulse parameters saved in header for Varian and Bruker files
+%
 %  ss_save_dyn(g,rf,ang,thk, format, fspec, a_angs, root_fname, pulse_num)
 %
 %  g - in G/cm    
@@ -12,7 +12,7 @@ function ss_save_dyn(g,rf,ang,thk, isodelay, format, fspec, a_angs, root_fname, 
 %  thk - thickness in cm
 %  isodelay (optional) - delay from in-phase point to end of pulse (GE
 %  definition)
-%  format (optional) - 'GE' (default), 'Varian'  
+%  format (optional) - 'GE' (default), 'Varian', 'Bruker'
 %  fspec (optional) - frequency bands (Hz) to write in file
 %  a_angs (optional) - band amplidutes (radians) to write in file
 %  root_fname - root file name (no prompting)
@@ -47,13 +47,13 @@ end
         case 'GE'
             % force even number of samples due to some GE sequences having
             % issues loading odd number of samples
-            if rem(length(rf))
-                rf = [rf(:), 0];
-                g = [g(:), 0];
+            if rem(length(rf),2)
+                rf = [rf(:); 0];
+                g = [g(:); 0];
             end
             
         case 'Varian'
-            
+        case 'Bruker'
         otherwise
             error(sprintf(['Format save type of: %s not' ...
                 ' recognized'], format));
@@ -165,7 +165,7 @@ end
             fprintf(fid,'%10d \t\t #g_pwm \n',0);
             fprintf(fid,'%10d \t\t #g_pwm_abs \n',0);
             fprintf(fid,'# *************************************\n');
-            if (nargin == 7)
+            if (nargin  > 6)
                 for b = 1:length(a_angs)
                    fprintf(fid,'# Band %d: [%.2f, %.2f] Hz, %.2f degree flip\n', ...
                        b, fspec(2*b-1), fspec(2*b), a_angs(b)*180/pi);
@@ -197,7 +197,7 @@ end
             fprintf(fid,'# Resolution = %d us\n', SS_TS*1e6);
             fprintf(fid,'# Flip = %.2f degrees\n',ang*180/pi);
             fprintf(fid,'# Max B1 = %.4f Gauss\n',max_b1);
-             if (nargin == 7)
+             if (nargin > 6)
                 for b = 1:length(a_angs)
                    fprintf(fid,'# Band %d: [%.2f, %.2f] Hz, %.2f degree flip\n', ...
                        b, fspec(2*b-1), fspec(2*b), a_angs(b)*180/pi);
@@ -232,7 +232,142 @@ end
             fprintf(fid,'%d  1\n',round(32767*gn));
             fclose(fid);
     
+
+
+        case 'Bruker'
+            
+            fid = fopen(sprintf('%s%03d.exc', root_fname, pulse_num),'wt');
+  
+            fprintf(fid,'##TITLE= %s\n', sprintf('%s%03d.RF', root_fname, pulse_num));
+            fprintf(fid,'##JCAMP-DX= 5.00 BRUKER JCAMP library\n');
+            fprintf(fid,'##DATA TYPE= Shape Data\n');
+            fprintf(fid,'##ORIGIN= BRUKER MEDICAL\n');
+            fprintf(fid,'##OWNER= <BRUKER MEDICAL>\n');
+            fprintf(fid,'##DATE= 20171106\n');
+            fprintf(fid,'##MINX= \n');
+            fprintf(fid,'##MAXX= \n');
+            fprintf(fid,'##MINY= \n');
+            fprintf(fid,'##MAXY= \n');
+            fprintf(fid,'##$SHAPE_EXMODE=  Excitation\n');
+            fprintf(fid,'##$SHAPE_TOTROT= 90.000000e+00\n');
+            fprintf(fid,'##$SHAPE_BWFAC= %6.6f\n',z_tb);
+            fprintf(fid,'##$SHAPE_INTEGFAC=      %1.4f\n',  sum(cos(-angle(rfn)).*abs(rfn))/length(rfn)); %changed by Vickie for Varian 
+
+            fprintf(fid,'##$SHAPE_REPHFAC= 50\n');
+            fprintf(fid,'##$SHAPE_TYPE=conventional\n');
+            fprintf(fid,'##$SHAPE_MODE= 0 \n');
+            fprintf(fid,'##NPOINTS= %d\n',length(g)); 
+            
+            fprintf(fid,'##Spectral_spatial_Matlab_Package\n');
+            fprintf(fid,'##Nucleus= %s\n',SS_NUCLEUS);
+            fprintf(fid,'##Duration= %d us\n',round(length(rf)*SS_TS*1e6));
+            fprintf(fid,'##Resolution= %d us\n', SS_TS*1e6);
+            fprintf(fid,'##Flip= %.2f degrees\n',ang*180/pi);
+            fprintf(fid,'##Max_B1 = %.4f Gauss\n',max_b1);
+            fprintf(fid,'##Spatial_time_bandwidth= %d\n',z_tb);
+            if SS_SLR_FLAG == 0
+               fprintf(fid,'##SHAPE= small tip\n');
+            else
+               fprintf(fid,'##SHAPE= SLR\n');
+            end
+             if (nargin > 6)
+                for b = 1:length(a_angs)
+                   fprintf(fid,'##Band_%d= [%.2f, %.2f] Hz, %.2f degree flip, %.3f ripple\n', ...
+                       b, fspec(2*b-1), fspec(2*b), a_angs(b)*180/pi, d(b)/sin(max(a_angs)));
+                end
+            end             
+            fprintf(fid,'##XYPOINTS= (XY..XY)\n');
+            
+            %fprintf(fid,'%3.2f  %4.2f  1.0\n',[-angle(rfn(:)).'*180/pi; 1023*abs(rfn(:)).']);    
+            %fprintf(fid,'%5.2f  %5.2f \n',[-angle(rfn(:)).'*180/pi; 1023*abs(rfn(:)).']);    
+            fprintf(fid,'%5.2f, %5.2f \n',[abs(rfn(:)).'*100'; -angle(rfn(:)).'*180/pi' ]);    
+       
+
+            fclose(fid);
+
+            fid = fopen(sprintf('%s%03d.gp', root_fname, pulse_num),'wt');
+            fprintf(fid,'##TITLE= %s\n', sprintf('%s%03d.gp', root_fname, pulse_num));
+            fprintf(fid,'## Spectral_spatial_Matlab_Package\n');
+            fprintf(fid,'##Nucleus= %s\n',SS_NUCLEUS);
+            fprintf(fid,'##SHAPE= %s\n',ss_type);
+            fprintf(fid,'##Duration= %d us\n',round(length(g)*SS_TS*1e6));
+            fprintf(fid,'##Resolution= %d us\n', SS_TS*1e6);
+            fprintf(fid,'##Points= %d\n',length(g));
+            fprintf(fid,'##Max_Gradient_Strength= %.4f Gauss/cm\n',maxg);
+            fprintf(fid,'##(Max_Gradient_Strength_Constraint= %.2f Gauss/cm)\n',SS_MXG);
+            fprintf(fid,'##Max_Slew Rate= %.2f Gauss/cm/ms\n',SS_MXS);
+            fprintf(fid,'##Slice_thickness= %.1f mm\n',thk*10);
+            fprintf(fid,'%5.3f  \n',g);
+            fclose(fid);
     end
+    
+    % Data Acquisition Descriptor (DAD) XML file containing RF pulse information
+    % for more info see SIVIC project https://github.com/SIVICLab/sivic
+    dad_name = sprintf('%s.xml', root_fname);
+    
+    if pulse_num ==1
+        docNode = com.mathworks.xml.XMLUtils.createDocument('svk_data_acquisition_description');
+        dad = docNode.getDocumentElement;
+        
+        dad_version = docNode.createElement('version');
+        dad_version.appendChild(docNode.createTextNode('0'));
+        dad.appendChild(dad_version);
+        
+        encoding =docNode.createElement('encoding');
+        dad.appendChild(encoding);
+        
+        excitation =docNode.createElement('excitation');
+        encoding.appendChild(excitation);
+        
+        spectralType =docNode.createElement('spectralType');
+        spectralType.appendChild(docNode.createTextNode('selective'));
+        excitation.appendChild(spectralType);
+        
+        spatialType =docNode.createElement('spatialType');
+        spatialType.appendChild(docNode.createTextNode('selective'));
+        excitation.appendChild(spatialType);
+        
+        pulseName =docNode.createElement('pulseName');
+        pulseName.appendChild(docNode.createTextNode(root_fname));
+        excitation.appendChild(pulseName);
+        
+        % flip angle(s) and associated frequency bands
+        if (nargin > 6)
+            for b = 1:length(a_angs)
+                curr_node = docNode.createElement('flipAngle_deg');
+                
+                % need to convert to strings??
+                curr_node.setAttribute('frequencyMin_Hz',num2str(min(fspec(2*b-1),fspec(2*b))));
+                curr_node.setAttribute('frequencyMax_Hz',num2str(max(fspec(2*b-1),fspec(2*b))));
+                
+                curr_node.appendChild(docNode.createTextNode(num2str(a_angs(b)*180/pi)));
+                
+                excitation.appendChild(curr_node);
+            end
+        end
+        
+    else
+        docNode = xmlread(dad_name);
+        
+        % flip angle(s) and associated frequency bands
+        if (nargin > 6)
+            flipAngle_deg = docNode.getElementsByTagName('flipAngle_deg');
+            
+            for b = 1:length(a_angs)
+                curr_node = flipAngle_deg.item(b-1);
+                curr_node_child = curr_node.getFirstChild;
+                new_flipAngle_deg_data = [char(curr_node_child.getData), ' ' num2str(a_angs(b)*180/pi)];
+                curr_node.replaceChild(docNode.createTextNode(new_flipAngle_deg_data), curr_node_child);
+            end
+        end
+        
+    end
+    
+    
+    % add pulse frequency
+    
+    xmlwrite(dad_name,docNode);
+    
     
     
     
