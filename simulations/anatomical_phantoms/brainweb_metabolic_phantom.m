a314c3ae0c18bdc3d71b99ef53ec3a58209cf212
Sule Sahin <sisahin8@gmail.com>
2024-03-15 16:26:32 -0700

    added kTRANS gradient functionality

--- simulations/anatomical_phantoms/brainweb_metabolic_phantom.m
+++ simulations/anatomical_phantoms/brainweb_metabolic_phantom.m
@@ -1,4 +1,4 @@
-function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, ktransScales, isFuzzy, matSize, simParams, augmentParams, augmentSeed)
+function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, ktransScales, isFuzzy, matSize, simParams, linear_kTRANS_grad, augmentParams, augmentSeed)
 % BRAINWEB_METABOLIC_PHANTOM generates standardized 3-dimensional perfusion
 %   and metabolism maps for simulated experiments. Supports 3 chemical pool
 %   kinetic rate mapping.
@@ -8,6 +8,8 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
 %                         pools) - 1, 3 (tissue types: vasc, gm, wm)]
 %       ktransScales    = volume transfer constants for different tissue
 %                         compartments [vasculature, gray matter, white matter]
+%                         if linear_kTRANS_grad=true, should include low
+%                         and high kTRANS [[vasc_low, GM_low, WM_low]; [vasc_hi, GM_hi, WM_hi]]
 %       isFuzzy         = logical flag to indicate if fuzzy tissue boundaries
 %                         will be used, default = true
 %       matSize         = 1x3 vector for desired matrix size of each dimension
@@ -15,6 +17,8 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
 %       simParams       = parameters used for the kinetic simulations: Mz0, 
 %                         Tarrival, Tbolus, TR, Nt, R1, flips; if empty won't
 %                         generate metImages, default = empty struct
+%  linear_kTRANS_grad   = boolean flag for whether kTRANS should be set to 
+%                         a gradient
 %       augmentParams   = random augmentation parameters XTranslation, 
 %                         YTranslation etc, if not defined a single 
 %                         unaugmented phantom will be generated,
@@ -30,14 +34,17 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
 %       Jasmine Hu
 %       Anna Bennett
 %       Sule Sahin
+%
+% Copyright, 2024
 
     % parse input arguments
     arguments
         kineticRates (:,3) double {mustBeNumeric} = [0.1, 0.2, 0.3; 0, 0, 0]
-        ktransScales (1,3) double {mustBeNumeric} = [1, 0.3, 0.3]
+        ktransScales (:,3) double {mustBeNumeric} = [1, 0.3, 0.3]
         isFuzzy double {mustBeNumericOrLogical} = true
         matSize (1,3) double {mustBeInteger} = [16, 16, 8]
         simParams struct = struct([])
+        linear_kTRANS_grad double {mustBeNumericOrLogical} = false
         augmentParams struct = struct()
         augmentSeed double {mustBeInteger, mustBePositive, mustBeNonzero} = []
     end
@@ -46,6 +53,14 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
         rng(augmentSeed)
     else
         rng("shuffle")
+    end
+
+    if and(linear_kTRANS_grad, size(ktransScales,1)==1)
+        ktransScales = repmat(ktransScales, [2 1]);
+        warning("kTRANS linear gradient is 'true' but low/high kTRANS values not defined. Using user-defined kTRANS values as both low and high.")
+    elseif and(~linear_kTRANS_grad, size(ktransScales,1)>1)
+        ktransScales = squeeze(ktransScales(1,:));
+        warning("kTRANS linear gradient is 'false' but low/high kTRANS values defined. Using first row of kTRANS values.")
     end
     
     % define default augmentation parameters and set augmentParams to them
@@ -74,10 +89,11 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
     end
     
     load(fullfile(baseMaskFile.folder,brainwebFile),'im_mask');
-    vasc_mask = squeeze(im_mask(:,:,1,:));
+    vasc_mask = squeeze(im_mask(:,:,:,1));
     maskSize = size(im_mask,1:3);
-    gm_mask = squeeze(im_mask(:,:,2,:));
-    wm_mask = squeeze(im_mask(:,:,3,:));
+    gm_mask = squeeze(im_mask(:,:,:,2));
+    wm_mask = squeeze(im_mask(:,:,:,3));
+    brain_mask = vasc_mask + gm_mask + wm_mask;
     
     k_1_2 = kineticRates(1,:);
     k_1_3 = kineticRates(2,:);
@@ -90,9 +106,22 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
     % parameters for output map generation
     permuted_mask = permute(im_mask,[4 1 2 3]);
     sumWeights = sum(im_mask,4);
+
+    if linear_kTRANS_grad 
+        % generate kTRANS gradients per compartment
+        grad_vasc = generate_linear_gradient(maskSize, ktransScales(1,1), ktransScales(2,1));
+        grad_gm = generate_linear_gradient(maskSize, ktransScales(1,2), ktransScales(2,2));
+        grad_wm = generate_linear_gradient(maskSize, ktransScales(1,3), ktransScales(2,3));
+        
+        % generate the kTRANS masked volume
+        kTRANS_vasc = squeeze(permuted_mask(1,:,:,:)) .* grad_vasc;
+        kTRANS_gm = squeeze(permuted_mask(2,:,:,:)) .* grad_gm;
+        kTRANS_wm = squeeze(permuted_mask(3,:,:,:)) .* grad_wm;
+        kTRANS_wSum = kTRANS_vasc + kTRANS_gm + kTRANS_wm;
+    else
+        kTRANS_wSum = pagemtimes(ktransScales,permuted_mask);
+    end
     
-    % generate the kTRANS masked volume
-    kTRANS_wSum = pagemtimes(ktransScales,permuted_mask);
     kTRANS = squeeze(kTRANS_wSum)./sumWeights;
     kTRANS(isnan(kTRANS)) = 0;
     
@@ -104,11 +133,11 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
     k_1_2_MAP(isnan(k_1_2_MAP)) = 0;
     k_1_3_MAP(isnan(k_1_3_MAP)) = 0;
     
-    % resample/downsample maps to desired size
+    % resample/downsample maps to desired size in x/y
     kTRANS = imresize3(kTRANS, matSize);
     k_1_2_MAP = imresize3(k_1_2_MAP, matSize);
     k_1_3_MAP = imresize3(k_1_3_MAP, matSize);
-    
+   
     % Random Augmentations (requires image processing toolbox)
     tform = randomAffine2d('Rotation',augmentParams.Rotation, 'Scale', ...
         augmentParams.Scale, 'XReflection', augmentParams.XReflection, ...
@@ -145,4 +174,13 @@ function [kTRANS, kMaps, metImages] = brainweb_metabolic_phantom(kineticRates, k
         metImages = 0;
     end
 
+end
+
+function [grad] = generate_linear_gradient(maskSize, kTRANS_low, kTRANS_high)
+
+    x = linspace(-1, 1, maskSize(1));
+    y = linspace(-1, 1, maskSize(2));
+    z = linspace(-1, 1, maskSize(3));
+    [X, Y, Z] = meshgrid(x, y, z);
+    grad = 0.5*(kTRANS_high - kTRANS_low)*Y + 0.5*(kTRANS_low + kTRANS_high);
 end
--- simulations/anatomical_phantoms/brainweb_metabolic_phantom_test.m
+++ simulations/anatomical_phantoms/brainweb_metabolic_phantom_test.m
@@ -2,10 +2,12 @@
 clear; close all;
 
 kineticRates = [0, 0.05, 0.03; 
-                0,    0,    0]; % no kPB, effectively, in order [vasc, GM, WM]
-ktransScales = [1, 0.3 0.3];
-isFuzzy = false;
-matSize = [16 16 8];
+                0, 0.02, 0.01]; % in order [vasc, GM, WM]
+ktransScales = [1, 0.2, 0.2;
+                3, 0.4, 0.4];
+isFuzzy = true;
+matSize = [16 16 16];
+linear_kTRANS_grad = true;
 
 % define simulation parameters: Mz0, Tarrival, Tbolus, TR, Nt, R1, flips,
 % std_noise
@@ -15,20 +17,20 @@ simParams.Tbolus = 8;
 simParams.TR = 4;
 simParams.Nt = 30;
 simParams.R1 = [1/30 1/25 1/25];
-simParams.flips = ones(3,simParams.Nt)*20*pi/180;
-simParams.std_noise = 0.003; 
+simParams.flips = repmat([20; 30; 30],[1 simParams.Nt])*pi/180;
+simParams.std_noise = 0.005; 
 
 % define augmentation parameters
 augmentParams.XTranslation = [-1 1];
-augmentParams.Scale = [0.9 1.1];
+augmentParams.Scale = [0.95 1.1];
 augmentParams.XReflection = true;
 augmentParams.Rotation = [-5 5];
 
-[k_trans, k_maps, metImages] = brainweb_metabolic_phantom(kineticRates, ktransScales, isFuzzy, matSize, simParams, augmentParams);
+[k_trans, k_maps, metImages] = brainweb_metabolic_phantom(kineticRates, ktransScales, isFuzzy, matSize, simParams, linear_kTRANS_grad, augmentParams);
 
 %% visualize kTRANS and k maps
 
-slices = 1:matSize(3);
+slices = 1:size(k_trans,3);
 tpts = 1:3:simParams.Nt;
 
 % visualize kTRANS
@@ -39,8 +41,8 @@ imagescn(k_trans(:,:,slices),[0 max(k_trans(:,:,slices),[],'all')], [1 numel(sli
 figure,
 imagescn(k_maps(:,:,slices,1),[0 max(k_maps(:,:,slices,1),[],'all')], [1 numel(slices)]); colormap fire;
 
-%figure, % picking an arbitrarily small number for the lower scale bound since k13 == 0
-%imagescn(k_maps(:,:,slices,2),[0 max(k_maps(:,:,slices,2),[],'all')], [1 numel(slices)]); colormap fire;
+figure, 
+imagescn(k_maps(:,:,slices,2),[0 max(k_maps(:,:,slices,2),[],'all')], [1 numel(slices)]); colormap fire;
 
 %% visualize metImages
 
@@ -56,18 +58,18 @@ imagescn(squeeze(metImages(:,:,slices,2,tpts)),[0 max(squeeze(metImages(:,:,slic
 figure,
 imagescn(squeeze(metImages(:,:,slices,3,tpts)),[0 max(squeeze(metImages(:,:,slices,3,tpts)),[],'all')], [numel(slices) numel(tpts)]); colormap fire;
 
-% visualize AUCs
+%% visualize AUCs
 %pyruvate
-pyrAUC = sum(squeeze(metImages(:,:,:,1,:)),4);
+pyrAUC = sum(squeeze(metImages(:,:,:,1,:)),length(size(squeeze(metImages(:,:,:,1,:)))));
 figure,
 imagescn(pyrAUC,[0 max(pyrAUC,[],'all')], [1 numel(slices)]); colormap fire;
 
 %lactate
-lacAUC = sum(squeeze(metImages(:,:,:,2,:)),4);
+lacAUC = sum(squeeze(metImages(:,:,:,2,:)),length(size(squeeze(metImages(:,:,:,2,:)))));
 figure,
 imagescn(lacAUC,[0 max(lacAUC,[],'all')], [1 numel(slices)]); colormap fire;
 
 %bicarb
-bicAUC = sum(squeeze(metImages(:,:,:,3,:)),4);
+bicAUC = sum(squeeze(metImages(:,:,:,3,:)),length(size(squeeze(metImages(:,:,:,3,:)))));
 figure,
 imagescn(bicAUC,[0 max(bicAUC,[],'all')], [1 numel(slices)]); colormap fire;
