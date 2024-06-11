dd2fb48ee400a62b43371ce03e1d55cff3d14787
Peder Larson <peder.larson@ucsf.edu>
2024-06-11 16:05:05 -0700

    Updates to singleband C1 pyruvate pulse demo
    Fixed feature that supported incidental excitation of other metabolites
    clarified definition of frequency bnads

--- RF_pulses/spectral_spatial/examples/singleband_C1_Pyr_demo.m
+++ RF_pulses/spectral_spatial/examples/singleband_C1_Pyr_demo.m
@@ -1,12 +1,14 @@
 % Script to generate a singleband SPSP RF pulse, suitable for imaging
 % pyruvate, lactate, and bicarbonate in an HP 1-13C pyruvate experiment
 
+
+% Set SS package globals
 close all, clearvars; clc
 ss_globals;
 ss_opt([]);				% Reset all options
 opt = ss_opt({'Nucleus', 'Carbon', ...
 	      'Max Duration', 20.5e-3, ...
-	      'Num Lobe Iters', 15, ...
+	      'Num Lobe Iters', 8, ...
 	      'Max B1', 1.6, ...
 	      'Num Fs Test', 100, ...
 	      'Verse Fraction', 0.7, ...
@@ -17,7 +19,7 @@ opt = ss_opt({'Nucleus', 'Carbon', ...
           'Max Grad',5,...
           'Max Slew',20});
 
-% SPECTRAL PULSE PARAMETERS 
+%% SPECTRAL PULSE PARAMETERS 
 % Design a pulse suitable for imaging C1 pyruvate, lactate, and bicarbonate
 % at 3 T
 %
@@ -25,12 +27,30 @@ opt = ss_opt({'Nucleus', 'Carbon', ...
 % Note that freq bandwidth is +/-, NOT TOTAL. Need to double to get total bandwidth.
 % To generate a pulse suitable for imaging all metabolites, for best
 % results place a broad symmetric stopband around the central resonance
+
+disp(['Here''s a C13 single-band excitation pulse for a [1-13C]pyruvate experiment ' ...
+    'suitable for imaging C1 pyruvate, lactate, and bicarbonate.  ' ...
+    'For best results it places a broad symmetric stopband around the central resonance']);
+
 B0 = 3e4; % G
 c13ppm = 1e-6 * B0 * SS_GAMMA; % 1 ppm = gamma_C13 * B0 * 1e-6
-% metabolite                    frequency (Hz)          freq bandwidth (Hz)         flip angle (deg)	allowed ripple
-mets(1).name = 'passband';      mets(1).f = 0;          mets(1).df = 1*c13ppm;      mets(1).ang = 90; 	mets(1).d = .01;
-mets(2).name = 'stopband1'; 	mets(2).f = 9*c13ppm; 	mets(2).df = 5*c13ppm; 		mets(2).ang = 0; 	mets(2).d = .005;
-mets(3).name = 'stopband2'; 	mets(3).f = -9*c13ppm;  mets(3).df = 5*c13ppm; 		mets(3).ang = 0; 	mets(3).d = .005;
+
+% 3T frequency shifts for C1-pyruvate studies
+freq_c1 = [395   270   180     0  -322]; %Lac, Pyr-Hydrate, Ala, Pyr, Bic
+metabolite_c1 = {'Lactate','Pyruvate-hydrate','Alanine','Pyruvate','Bicarbonate'};
+% minimum metabolite separation - using lac to pyr-hyd
+fmin_stopband = freq_c1(1) - freq_c1(2);
+% maximum metabolite separation, and adding 1 ppm buffer
+fmax_stopband = freq_c1(1) - freq_c1(5) + 1*c13ppm;
+
+fcenter_stopband = (fmin_stopband + fmax_stopband)/2;
+df_stopband = (fmax_stopband - fmin_stopband)/2;
+
+% metabolite                    frequency (Hz)                  freq bandwidth (Hz)         flip angle (deg)	allowed ripple
+mets(1).name = 'passband';      mets(1).f = 0;                  mets(1).df = 1*c13ppm;      mets(1).ang = 90; 	mets(1).d = .02;
+mets(2).name = 'stopband1'; 	mets(2).f = fcenter_stopband; 	mets(2).df = df_stopband; 	mets(2).ang = 0; 	mets(2).d = .005;  % low ripple here important to not excite the large pyruvate when exciting other metabolites
+mets(3).name = 'stopband2'; 	mets(3).f = -fcenter_stopband;  mets(3).df = df_stopband; 	mets(3).ang = 0; 	mets(3).d = .005; % low ripple here important to not excite the large pyruvate when exciting other metabolites
+
 
 % create vectors of angles, ripples, and band edges for input to pulse design
 [fspec, a_angs, d] = create_freq_specs(mets, 0);
@@ -51,7 +71,7 @@ z_ftype = 'ls';            %spatial filter
 z_d1 = 0.01;               %passband ripple
 z_d2 = 0.01;               %stopband ripple
 
-% 1) Fs:  868.1 B1: 0.223G Power: 3.051e-05 G^2 ms Dur: 20.6m
+% 1) Fs:  868.1 B1: 0.208G Power: 2.831e-05 G^2 ms Dur: 20.6ms
 [g,rf,fs] = ...
     ss_design(z_thk, z_tb, [z_d1 z_d2], fspec, a_angs, d, ptype, ...
 	      z_ftype, [], ss_type, fctr, 0);
@@ -82,22 +102,38 @@ fplot = freq_c1 - freq_c1(3);
 ss_plot(g, rf, SS_TS, ptype, z_thk*3, 2.5*[min(fspec) max(fspec)], SS_GAMMA, fplot);
 set(gcf,'Name','C1 Alanine Excitation','NumberTitle','Off')
 
+fprintf(1,'Hit any key to continue:\n');
+pause;
+
 %% Adapt singleband SPSP for alanine
-% As seen above, the passband FWHM was too broad to selectively excite
-% alanine. To create one that is sufficiently narrow, we will need to reduce
-% the passband FWHM and increase the Max Duration in the SS_OPT structure, 
-% since the total pulsewidth will ultimately limit the passband response 
-% (for Gaussiun modulation, minimum passband FWHM = 2/pw).
+disp(['As seen above, the passband FWHM was too broad to selectively excite alanine. ' ...
+    'To create one that is sufficiently narrow, we will need to reduce the passband FWHM ' ...
+    'and increase the Max Duration in the SS_OPT structure, ' ...
+    'since the total pulsewidth will ultimately limit the passband response ' ...
+    '(for Gaussiun modulation, minimum passband FWHM = 2/pw).'])
+
 opt = ss_opt({'Max Duration', 24e-3});
 mets(1).df = 0.5*c13ppm;
+
 % Increase stopband width to help suppress ripples from the passband transition
-mets(2).df = 6*c13ppm;
-mets(3).df = 6*c13ppm;
+
+% minimum metabolite separation - pyr-hyd to ala
+fmin_stopband = freq_c1(2) - freq_c1(3);
+% maximum metabolite separation - lac to bic, and adding 1 ppm buffer
+fmax_stopband = freq_c1(1) - freq_c1(5) + 1*c13ppm;
+
+fcenter_stopband = (fmin_stopband + fmax_stopband)/2;
+df_stopband = (fmax_stopband - fmin_stopband)/2;
+
+% metabolite                    frequency (Hz)                  freq bandwidth (Hz)         flip angle (deg)	allowed ripple
+mets(2).name = 'stopband1'; 	mets(2).f = fcenter_stopband; 	mets(2).df = df_stopband; 	mets(2).ang = 0; 	mets(2).d = .005;  % low ripple here important to not excite the large pyruvate when exciting other metabolites
+mets(3).name = 'stopband2'; 	mets(3).f = -fcenter_stopband;  mets(3).df = df_stopband; 	mets(3).ang = 0; 	mets(3).d = .005; % low ripple here important to not excite the large pyruvate when exciting other metabolites
+
 % Regenerate passband & stopband specifications
 [fspec, a_angs, d] = create_freq_specs(mets, 0); 
 
 close all;
-% 1) Fs:  868.1 B1: 0.141G Power: 1.765e-05 G^2 ms Dur: 24.0msms
+% 1) Fs:  868.1 B1: 0.134G Power: 1.634e-05 G^2 ms Dur: 24.0ms
 [g,rf,fs] = ...
     ss_design(z_thk, z_tb, [z_d1 z_d2], fspec, a_angs, d, ptype, ...
 	      z_ftype, [], ss_type, fctr, 0);
@@ -126,3 +162,5 @@ set(gcf,'Name','Bicarbonate Excitation','NumberTitle','Off')
 fplot = freq_c1 - freq_c1(3);
 ss_plot(g, rf, SS_TS, ptype, z_thk*3, 2.5*[min(fspec) max(fspec)], SS_GAMMA, fplot);
 set(gcf,'Name','C1 Alanine Excitation','NumberTitle','Off')
+
+
